8bc0720ecfa87c0c1d31e59eb45216fd64b2ced6
ias758 <77140326+ias758@users.noreply.github.com>
2021-04-13 11:05:05 +0700

    Update task_12_1.py

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,22 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+
+addresses = ['8.8.8.8', '255.254.252.1', '10.110.1.1']
+
+
+def ping_ip_addresses(address):
+    reachable = []
+    unreachable = []
+    for ip in address:
+        reply = subprocess.run(['ping', '-n', '3', ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if reply.returncode == 0:
+            reachable.append(ip)
+        else:
+            unreachable.append(ip)
+    return [reachable, unreachable]
+
+
+if __name__ == "__main__":
+    print(ping_ip_addresses(addresses))   
