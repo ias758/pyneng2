3c25f798eed4cceb32eec3e9bfb148a0d7667dd7
ias758 <77140326+ias758@users.noreply.github.com>
2021-07-07 11:04:26 +0700

    Update task_17_2.py

--- exercises/17_serialization/task_17_2.py
+++ exercises/17_serialization/task_17_2.py
@@ -49,3 +49,69 @@ sh_version_files = glob.glob("sh_vers*")
 # print(sh_version_files)
 
 headers = ["hostname", "ios", "image", "uptime"]
+def write_inventory_to_csv(data_filenames, csv_filename):
+    ios = r'.+ Version (\d+\.\S+),'
+    image = r'System .+ \"(\S+)\"'
+    uptime = r'router uptime is (.+)\n'
+    data2 = []
+    for file in sh_version_files:
+        host = re.search(r'version_(\S+).txt$', file)
+        with open(file) as f:
+            data = dict.fromkeys(headers)
+            for line in f:
+                match_ios = re.search(ios, line)
+                match_image = re.search(image, line)
+                match_uptime = re.search(uptime, line)
+                data['hostname'] = host.group(1)
+                if match_ios:
+                    data['ios'] = match_ios.group(1)
+                elif match_image:
+                    data['image'] = match_image.group(1)
+                elif match_uptime:
+                    data['uptime'] = match_uptime.group(1)
+            data2.append(data)
+    with open(csv_filename,'w') as dst:
+        writer = csv.DictWriter(dst, fieldnames = headers)
+        writer.writeheader()
+        for d in data2:
+            writer.writerow(d)
+                    
+if __name__ == "__main__":
+    write_inventory_to_csv(sh_version_files, 'out_17_2.csv' )
+
+#Answer
+'''
+import re
+import csv
+import glob
+
+
+def parse_sh_version(sh_ver_output):
+    regex = (
+        "Cisco IOS .*? Version (?P<ios>\S+), .*"
+        "uptime is (?P<uptime>[\w, ]+)\n.*"
+        'image file is "(?P<image>\S+)".*'
+    )
+    match = re.search(regex, sh_ver_output, re.DOTALL,)
+    if match:
+        return match.group("ios", "image", "uptime")
+
+
+def write_inventory_to_csv(data_filenames, csv_filename):
+    headers = ["hostname", "ios", "image", "uptime"]
+    with open(csv_filename, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+
+        for filename in data_filenames:
+            hostname = re.search("sh_version_(\S+).txt", filename).group(1)
+            with open(filename) as f:
+                parsed_data = parse_sh_version(f.read())
+                if parsed_data:
+                    writer.writerow([hostname] + list(parsed_data))
+
+
+if __name__ == "__main__":
+    sh_version_files = glob.glob("sh_vers*")
+    write_inventory_to_csv(sh_version_files, "routers_inventory.csv")
+'''
