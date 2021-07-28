643eba7f5b03acaa94e9bd8acdfceaeec8ee3c6b
ias758 <77140326+ias758@users.noreply.github.com>
2021-07-28 10:08:31 +0700

    Update task_18_2c.py

--- exercises/18_ssh_telnet/task_18_2c.py
+++ exercises/18_ssh_telnet/task_18_2c.py
@@ -51,7 +51,73 @@ In [12]: pprint(result)
 """
 
 # списки команд с ошибками и без:
-commands_with_errors = ["logging 0255.255.1", "logging", "a"]
-correct_commands = ["logging buffered 20010", "ip http server"]
+commands_with_errors = ['logging 0255.255.1', 'logging', 'a']
+correct_commands = ['interface f0/22', 'description Test_Script_2']
 
 commands = commands_with_errors + correct_commands
+
+import re
+import yaml
+
+from netmiko import (
+    ConnectHandler,
+    NetmikoAuthenticationException,
+    NetmikoTimeoutException,
+    )
+
+
+def break_if_no(i):
+    
+    while True:
+        answer = input('Продолжать выполнять команды? y/n: ')
+        
+        if answer == 'n':
+           return False
+        elif answer == 'y':
+            return True
+        else:
+            continue
+
+
+def send_config_commands(dev, commands, log = True):
+    
+    try:
+        regex = r'% (?P<err>.+)'
+        correct_commands = {}
+        incorrect_commands = {}
+        errmsg = 'Команда {} выполнилась с ошибкой {} на устройстве {}'
+
+        if log:
+            print(f"Подключение к {dev['host']}... ")
+
+        with ConnectHandler(**dev) as ssh:
+            ssh.enable()
+
+            for command in commands:
+                result = ssh.send_config_set(command, exit_config_mode=False)
+                error = re.search(regex, result)
+
+                if error:
+                    print(errmsg.format(command, error.group('err'), ssh.host))
+                    incorrect_commands[command] = result
+                    if break_if_no(command) is False:
+                        break
+                           
+                else:
+                    correct_commands[command] = result
+
+        result = (incorrect_commands, correct_commands)
+        return result
+    
+    except (NetmikoAuthenticationException, NetmikoTimeoutException ) as error:
+        print(error)
+        
+    
+
+if __name__ == "__main__":
+    #commands = ['interface f0/22', 'description Test_Script_2']
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+
+    for dev in devices:
+        print(send_config_commands(dev, commands))
